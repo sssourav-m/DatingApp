37eb9d14c785f070b01da4fdd76459440ca0e167
sssourav-m <sourav.maji.54321@gmail.com>
2022-04-28 19:13:45 +0530

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+
+    StringBuilder sb = new StringBuilder();
+
+}
